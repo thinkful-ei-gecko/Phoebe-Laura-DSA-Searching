54609abac80fb20546b15e943c629df8a323d462
Phoebe Law <phoebebasilio@gmail.com>
2019-11-13 12:46:48 -0800

    adds edits

--- drills.js
+++ drills.js
@@ -110,6 +110,10 @@ console.log(findBook('005.133', 'The C++ Programming Language', library)) // aut
 // ** No coding is needed for these drills**. Once you have answered it, you can then code the tree and implement the traversal to see if your answer is correct.
 
 // 1) Given a binary search tree whose in-order and pre-order traversals are respectively 14 15 19 25 27 35 79 89 90 91 and 35 25 15 14 19 27 89 79 91 90. What would be its postorder traversal?
+const inorder = [14, 15, 19, 25, 27, 35, 79, 89, 90, 91];
+const preOrder = [35, 25, 15, 14, 19, 27, 89, 79, 91, 90];
+
+const postOrder = [, 25, 79, 91, 89, 35]
 
 // 2) The post order traversal of a binary search tree is 5 7 6 9 11 10 8. What is its pre-order traversal?
 
