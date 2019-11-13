51c0d368fd9e74394fce5134db461f6c347e61c5
Phoebe Law <phoebebasilio@gmail.com>
2019-11-13 11:13:48 -0800

    Adds 1-2

---
+++ drills.js
@@ -0,0 +1,11 @@
+// 1. How many searches?
+  // Q: Given a sorted list 3, 5, 6, 8, 11, 12, 14, 15, 17, 18 and using the recursive binary search algorithm, identify the sequence of numbers that each recursive call will search to try and find 8.
+  // A: 11, 5, 6, 8 (n=4)
+  // Q: Given a sorted list 3, 5, 6, 8, 11, 12, 14, 15, 17, 18 and using the recursive binary search algorithm, identify the sequence of numbers that each recursive call will search to try and find 16.
+  // A: 11, 15, 17; returns -1 (16 not found)
+
+  
+// 2. Adding a React UI
+
+// problem: input was taken in as a number (we said it was text)
+// couldn't get the regular way to work so i just made it a controlled form and set the state 
